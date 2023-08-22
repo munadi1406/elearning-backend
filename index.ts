9c1d227bce8498349528cf592b63b258b60270a7
munadi1406 <munadifathullah123@gmail.com>
2023-08-22 10:49:28 +0800

    oke

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import express, { Application } from 'express';
 import route from './routes/route';
 import "dotenv/config.js"
 import cors from 'cors'
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient()
 
 
 
@@ -12,14 +14,24 @@ app.use(cors({credentials:true,origin:"*"}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-try {
-  await prisma.$connect();
-  console.log('Terhubung ke database.');
-} catch (error) {
-  console.error('Gagal terhubung ke database:', error);
-} finally {
-  await prisma.$disconnect();
+async function start() {
+  try {
+    await prisma.$connect();
+    console.log('Terhubung ke database.');
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error('Gagal terhubung ke database:', error);
+  }
+  finally {
+    await prisma.$disconnect();
+  }
 }
+
+// Panggil fungsi start untuk memulai server
+start();
 
 app.get('/', (req, res) => {
     res.send("oke coy")
